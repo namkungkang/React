8e563b5e509fa16429a81f6ef48a96fd592e85d6
namkungkang <namkung0131@naver.com>
2024-07-08 21:07:54 +0900

    modify

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import Box from './components/Box';
 function App() {
   return (
     <div className="App">
-        <Box name="1" num="일"/>
-        <Box name="2" num="이"/>
-        <Box name="3" num="삼"/>
-        <Box name="4" num="사"/>
-        
+      
     </div>
   );
 }
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './Box.css';
+import Script from "./script";
 
 function Box(props) {
     return(
         <div className="box">
             {props.name}
             {props.num}
+            <Script/>
         </div>
 
 
