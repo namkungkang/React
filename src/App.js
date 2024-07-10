12c105d4f7cf15ad766b75efc6cf18abc3565e66
namkungkang <namkung0131@naver.com>
2024-07-10 22:47:15 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import Movie from './components/Movie';
-import { movies } from './movieDummy';
 
 function App() {
-  return (
-    <>
-      {movies.results.map((item) => (
-        <Movie
-        
-          poster_path={item.poster_path}
-          title={item.title}
-          vote_average={item.vote_average}
-          
-        />
-      ))}
-    </>
-  );
-}
+  const [count,setCount] = useState(1);
+  const [name,setName] = useState('');
 
-export default App;
+  const increase = () => {
+    setCount(count+1);
+  }
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  }
+
+  useEffect(()=> {
+    console.log("name 변화");
+  },[name])
+
+  //useEffect는 () 콜백함수로 받음 예시 (()=>{})
+  //useEffect는 마운팅 될 때만 랜더링하고싶다면 빈 배열
+  //name이 변화 될때만 랜더링하고 싶으면 배열안에 초기값
+  
+  return(
+    <div className='App'>
+        <button onClick={increase}>Update</button>
+        <span>count : {count}</span>
+        <input type='text' value={name} onChange={handleChange}></input>
+        <span>name: {name}</span>
+
+      
+    </div>
+
+
+
+  )
+
+}
+ export default App;
